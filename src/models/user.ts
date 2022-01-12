4738da601526d36915b48bdc4e70fa009189b05b
Hakula Chen <i@hakula.xyz>
2022-01-12 16:56:09 +0800

    feat(user): introduce mongoose-unique-validator to validate uniqueness of fields in user schema

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import mongooseUniqueValidator from 'mongoose-unique-validator';
 
 import { UserEntry } from '@/types';
 
@@ -6,11 +7,6 @@ const { model, Schema } = mongoose;
 
 const userSchema = new Schema<UserEntry>(
   {
-    userId: {
-      type: String,
-      required: true,
-      unique: true,
-    },
     email: {
       type: String,
       required: true,
@@ -25,12 +21,19 @@ const userSchema = new Schema<UserEntry>(
       required: true,
       default: false,
     },
+    isRemoved: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
   },
   {
     strictQuery: 'throw',
     timestamps: true,
   },
 );
+
+userSchema.plugin(mongooseUniqueValidator);
 
 const UserModel = model<UserEntry>('User', userSchema);
 export default UserModel;
