835b931d3b0475276ea08fa69ad925cc19f718c4
Hakula Chen <i@hakula.xyz>
2022-01-14 15:15:34 +0800

    chore(database): modify database connection configs

--- src/services/database.service.ts
+++ src/services/database.service.ts
@@ -10,7 +10,10 @@ export default class DatabaseManager {
       while (!this.db) {
         try {
           // eslint-disable-next-line no-await-in-loop
-          await mongoose.connect(`${DB_CONN_STRING}/${DB_NAME}`);
+          await mongoose.connect(`${DB_CONN_STRING}/${DB_NAME}`, {
+            minPoolSize: 10,
+            maxPoolSize: 64,
+          });
           this.db = mongoose.connection;
           console.log('[INFO ]', '(database)', `connected to database: ${this.db.name}`);
         } catch (error) {
@@ -23,10 +26,7 @@ export default class DatabaseManager {
     if (this.db) {
       this.db.on('connect', () => console.log('[INFO ]', '(database)', `connected to database: ${this.db!.name}`));
       this.db.on('open', () => console.log('[DEBUG]', '(database)', 'connection open'));
-      this.db.on('disconnect', async () => {
-        console.log('[INFO ]', '(database)', `disconnected from database: ${this.db!.name}`);
-        await tryConnect();
-      });
+      this.db.on('disconnect', () => console.log('[INFO ]', '(database)', 'disconnected from database'));
       this.db.on('close', () => console.log('[DEBUG]', '(database)', 'connection closed'));
       this.db.on('error', (error) => console.log('[WARN ]', '(database)', error));
     }
