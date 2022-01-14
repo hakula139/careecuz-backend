9879fd086321284ac7f7469b3221467b7efb42bb
Hakula Chen <i@hakula.xyz>
2022-01-15 05:48:22 +0800

    fix(notification): return a sorted query result

--- src/services/notificationCollection.service.ts
+++ src/services/notificationCollection.service.ts
@@ -4,7 +4,8 @@ import { NotificationModel } from '@/models';
 import { NotificationEntry } from '@/types';
 
 export const getNotifications = (toUserId: string | Types.ObjectId): Promise<HydratedDocument<NotificationEntry>[]> =>
-  NotificationModel.find({ toUserId }).populate('message').populate('replyToMessage').exec();
+  NotificationModel.find({ toUserId }).sort({ _id: -1 }).populate('message').populate('replyToMessage')
+    .exec();
 
 export const addNotification = (
   fromUserId: string | Types.ObjectId,
