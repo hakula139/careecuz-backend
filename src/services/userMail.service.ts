f0bbfe9ba950bc6c5abf66a924ada89a9660e2b7
Hakula Chen <i@hakula.xyz>
2022-01-17 12:07:31 +0800

    fix(verify-mail): modify mail subject & content

--- src/services/userMail.service.ts
+++ src/services/userMail.service.ts
@@ -7,9 +7,9 @@ export const sendVerifyCode = (to: string, verifyCode: string): Promise<[ClientR
   MailManager.send(
     to,
     MAIL_SENDER,
-    '欢迎来到 CareeCuz! 请验证您的邮箱～',
+    '欢迎来到 CareeCuz！请验证您的邮箱～',
     `
       <p>您的验证码是: <strong>${verifyCode}</strong></p>
-      <p>此验证码将在 1 小时后过期，请尽快填写 :P</p>
+      <p>此验证码将在 1 小时后过期，请尽快填写。</p>
     `,
   );
